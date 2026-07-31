0f4d2c9a93ade0634e038ff82b0d196dbeb0a0ca
lucianogiacomelli <lucianogiacomelli8@gmail.com>
2026-07-31 15:18:39 -0300

    Update Roles.java

--- src/main/java/com/Spring_Security/Roles_Permissions/Model/Roles.java
+++ src/main/java/com/Spring_Security/Roles_Permissions/Model/Roles.java
@@ -1,4 +1,28 @@
 package com.Spring_Security.Roles_Permissions.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
 public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String role;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "roles_permissions", joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private Set permissionSet = new HashSet<>();
 }
