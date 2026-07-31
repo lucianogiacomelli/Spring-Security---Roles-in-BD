0776101547beffb21a3f6df63a6d327828617282
lucianogiacomelli <lucianogiacomelli8@gmail.com>
2026-07-31 14:39:14 -0300

    initial commit

---
+++ src/main/java/com/Spring_Security/Roles_Permissions/RolesPermissionsApplication.java
@@ -0,0 +1,13 @@
+package com.Spring_Security.Roles_Permissions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolesPermissionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolesPermissionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Spring_Security/Roles_Permissions/RolesPermissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_Security.Roles_Permissions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolesPermissionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
