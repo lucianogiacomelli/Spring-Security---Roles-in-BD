04921f2f8363615e118ec2d5f10c3a5fb7e6a060
lucianogiacomelli <lucianogiacomelli8@gmail.com>
2026-07-31 15:08:54 -0300

    Update UserSec.java

--- src/main/java/com/Spring_Security/Roles_Permissions/Model/UserSec.java
+++ src/main/java/com/Spring_Security/Roles_Permissions/Model/UserSec.java
@@ -1,4 +1,34 @@
 package com.Spring_Security.Roles_Permissions.Model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserSec {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private boolean enabled;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set rolesList = new HashSet();
 }
