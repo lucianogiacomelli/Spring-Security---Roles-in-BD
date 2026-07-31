04e3ad27f67fcb6dc421ceeea6202ed8428762f1
lucianogiacomelli <lucianogiacomelli8@gmail.com>
2026-07-31 15:20:44 -0300

    Update Permissions.java

--- src/main/java/com/Spring_Security/Roles_Permissions/Model/Permissions.java
+++ src/main/java/com/Spring_Security/Roles_Permissions/Model/Permissions.java
@@ -1,4 +1,21 @@
 package com.Spring_Security.Roles_Permissions.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "permissions")
 public class Permissions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String permissionName;
 }
